a76b331db07ac95f1a1010cea20e60a0ac84fdc6
Ernesto Murillo <emurillo@email.neit.edu>
2017-11-06 20:42:01 -0500

    added lab 6

---
+++ public_html/week6/lab6/queryhandler.js
@@ -0,0 +1,74 @@
+
+var debug = require('debug')('lab6:queryHandler');
+
+function search(query) {
+    debug('search setup');
+
+    var where = {};  //changed
+    //added
+    var key;
+    for (key in query) {
+        if (key.indexOf('_') === -1) {
+            // (test1|test3) = .replace(/[\W_]+/g,'')
+            // where[key] =  { $regex: new RegExp('.*?'+req.query[key]+.replace(/[\W_]+/g,'''.*') };     /////or
+            where[key] = {$regex: new RegExp('.*?' + query[key] + '.*')};
+        }
+    }
+
+    return where;
+}
+
+function sort(query) {
+    debug('sort setup');
+    var sort = null;
+    if (query._sort) {
+        var prefix = 1;
+        if (query._sort.match(/-/))
+            prefix = -1;
+        var field = query._sort.replace(/-|\s/g, '');
+        sort = {};
+        sort[field] = prefix;
+    }
+    return sort;
+
+
+}
+
+//changed ***** from week 5 app.js
+module.exports.cors = function (reg, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+    next();
+};
+
+
+// enable Cross-Origin Resource Sharing (CORS) *****
+//app.use(function(reg, res, next){
+//    res.header('Access-Control-Allow-Origin', '*');
+//    res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept');
+//    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');   
+//    next();
+//});
+
+
+
+module.exports.search = function () {
+    return function (req, res, next) {
+        // Add the search functionality to the request object
+        req.where = search(req.query);
+
+        next();
+    };
+};
+
+module.exports.sort = function () {
+    return function (req, res, next) {
+        // Add the options sort functionality to the request object
+        if (!req.options)
+            req.options = {};
+        req.options.sort = sort(req.query);
+
+        next();
+    };
+};
---
+++ public_html/week6/lab6/routes/review.controller.js
@@ -0,0 +1,143 @@
+var Review = require('./review.model');
+var debug = require('debug')('lab6:employee');
+
+function sendJSONresponse(res, status, content) {
+    res.status(status);
+    res.json(content);
+};
+
+module.exports.employeesReadAll = function(req, res) {
+        
+    debug('Getting all employees');
+    if (req.query) {
+        debug(req.query);
+          
+    }
+    
+    Review
+     .find(req.where, null, req.options)  //changed where   changed options
+     .exec()
+     .then(function(results){
+        sendJSONresponse(res, 200, results);
+     })
+     .catch(function(err){
+        sendJSONresponse(res, 404, err);         
+     });
+    
+    
+//    Review
+//     .find()
+//     .exec()
+//     .then(function(results){
+//        sendJSONresponse(res, 200, results);
+//     })
+//     .catch(function(err){
+//        sendJSONresponse(res, 404, err);         
+//     });
+    
+};
+
+module.exports.employeesReadOne = function(req, res) {
+    
+    if (req.params && req.params.employeeid) {
+        debug('Getting single employee with id =', req.params.employeeid );
+        
+        Review
+        .findById(req.params.employeeid)
+        .exec()
+        .then(function(results){
+            sendJSONresponse(res, 200, results);
+        }).catch(function(err){
+            sendJSONresponse(res, 404, {
+                "message": "employeeid not found"
+            });
+        });
+
+    } else {
+        sendJSONresponse(res, 404, {
+            "message": "employeeid not found"
+        });
+    }
+};
+
+/*   POST a new review
+ *   /api/v1/reviews 
+ */
+module.exports.employeesCreate = function(req, res) {
+    
+    debug('Creating a employee with data ', req.body);
+    
+    Review.create({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        department: req.body.department,
+        startDate: req.body.startDate,
+        jobTitle: req.body.jobTitle,
+        salary: req.body.salary
+    })
+    .then(function(dataSaved){
+        debug(dataSaved);
+        sendJSONresponse(res, 201, dataSaved);
+    })
+    .catch(function(err){ 
+        debug(err);
+        sendJSONresponse(res, 400, err);
+    });
+     
+};
+
+module.exports.employeesUpdateOne = function(req, res) {
+    
+  if ( !req.params.employeeid ) {
+    sendJSONresponse(res, 404, {
+        "message": "Not found, employeeid is required"
+    });
+    return;
+  }
+  
+  Review
+    .findById(req.params.employeeid)
+    .exec()
+    .then(function(reviewData) {        
+        //reviewData.author = req.body.author;
+        //reviewData.rating = req.body.rating;
+        //reviewData.reviewText = req.body.reviewText;
+        reviewData.firstName = req.body.firstName;
+        reviewData.lastName = req.body.lastName;
+        reviewData.department = req.body.department;
+        reviewData.startDate = req.body.startDate;
+        reviewData.jobTitle = req.body.jobTitle;
+        reviewData.salary = req.body.salary;
+
+        return reviewData.save();
+    })
+    .then(function(data){
+        sendJSONresponse(res, 200, data);
+    })
+    .catch(function(err){
+        sendJSONresponse(res, 400, err);
+    });
+        
+};
+
+module.exports.employeesDeleteOne = function(req, res) {
+  if ( !req.params.employeeid ) {
+    sendJSONresponse(res, 404, {
+        "message": "Not found, employeeid is required"
+    });
+    return;
+  }
+  
+  Review
+    .findByIdAndRemove(req.params.employeeid)
+    .exec()
+    .then(function(data){
+        debug("employeeid id " + req.params.employeeid + " deleted");
+        debug(data);
+        sendJSONresponse(res, 204, null);
+    })
+    .catch(function(err){
+        sendJSONresponse(res, 404, err);
+    });
+    
+};
