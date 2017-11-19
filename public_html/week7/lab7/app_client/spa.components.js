99cfeb3cbe620760b3e660a5b3dc0828155f7953
Ernesto Murillo <emurillo@email.neit.edu>
2017-11-19 08:46:19 -0500

    added lab 7

---
+++ public_html/week7/lab7/app_client/spa.components.js
@@ -0,0 +1,19 @@
+class Components {
+
+   static employeeTable(data){
+        if ( !Array.isArray(data) ) return Promise.resolve('')
+        return Promise.resolve(`${data.map(row=>
+                    `<tr>
+                        <td>${row.firstName}</td>
+                        <td>${row.lastName}</td>
+                        <td>${row.department}</td>
+                        <td>${row.startDateFormatted}</td>
+                        <td>${row.jobTitle}</td>
+                        <td>${row.salaryFormatted}</td>
+                        <td><button data-id="${row.id}" data-bind-event="click:deleteEmployee" class="button is-danger is-outlined">Delete</button></td>
+                        <td><button data-id="${row.id}" data-bind-event="click:goToUpdatePage" class="button is-link is-outlined">Update</button></td>
+                    </tr>`
+                ).join('')}`)
+    }
+
+}
---
+++ public_html/week7/lab7/app_client/spa.controller.js
@@ -0,0 +1,20 @@
+class Controller {
+
+    constructor(model) {
+        this.Model = model
+    }
+
+    home() {
+        return this.Model.getEmployeeList()
+    }
+
+    add() {
+        this.Model.clearDataBindModel()
+        return window.Promise.resolve()
+    }
+
+    update() {
+        return this.Model.updatePageLoad()
+    }
+
+}
